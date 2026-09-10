c60ab8e18bb806b8e0fba16f6801a03a899d5c6d
a-l-a-z-a-r <natanimmayas@gmail.com>
2026-09-10 19:07:54 +0200

    Run 50 Kademlia containers

--- labs/main.go
+++ labs/main.go
@@ -5,13 +5,18 @@ package main
 import (
 	"d7024e/kademlia"
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Pretending to run the kademlia app...")
 	// Using stuff from the kademlia package here. Something like...
-	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000000000000000000000000000")
 	contact := kademlia.NewContact(id, "localhost:8000")
 	fmt.Println(contact.String())
 	fmt.Printf("%v\n", contact)
+
+	for {
+		time.Sleep(time.Hour)
+	}
 }
