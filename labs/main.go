db501b737548cb7ba4bfac280c56cdde69d56521
mohraj-0 <164861006+mohraj-0@users.noreply.github.com>
2026-09-12 13:54:13 +0200

    Uppdate main

--- labs/main.go
+++ labs/main.go
@@ -4,14 +4,44 @@ package main
 
 import (
 	"d7024e/kademlia"
-	"fmt"
+	"time"
 )
 
+//	func main() {
+//		fmt.Println("Pretending to run the kademlia app...")
+//		// Using stuff from the kademlia package here. Something like...
+//		id := kademlia.NewKademliaID("FFFFFFFF0000000000000000000000000000000000000000000000000000００００")
+//		contact := kademlia.NewContact(id, "localhost:8０００")
+//		fmt.Println(contact.String())
+//		fmt.Printf("%v\n", contact)
+//	}
+
 func main() {
-	fmt.Println("Pretending to run the kademlia app...")
-	// Using stuff from the kademlia package here. Something like...
-	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	contact := kademlia.NewContact(id, "localhost:8000")
-	fmt.Println(contact.String())
-	fmt.Printf("%v\n", contact)
+
+	// Startar en nod som lyssnar på UDP-port 8000
+	go kademlia.Listen("127.0.0.1", 8000)
+
+	// Nod B lyssnar på port 8001
+	go kademlia.Listen("127.0.0.1", 8001)
+
+	// Väntar lite så att listenern hinner starta
+	time.Sleep(1 * time.Second)
+
+	// Skapar ett ID för noden
+	id := kademlia.NewKademliaID(
+		"FFFFFFFF00000000000000000000000000000000000000000000000000000000",
+	)
+
+	// Skapar en kontakt som pekar på noden som lyssnar
+	contact := kademlia.NewContact(id, "127.0.0.1:8000")
+
+	// Skapar ett Network-objekt
+	network := kademlia.Network{}
+
+	// Skickar PING till noden
+	network.SendPingMessage(&contact)
+
+	// Väntar lite så att meddelandet hinner tas emot
+	time.Sleep(1 * time.Second)
+
 }
